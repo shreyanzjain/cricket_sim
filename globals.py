933768b5b6d2c084ea4ee5e027c03f54d42f6e68
shreyanzjain <shreyanz32@gmail.com>
2023-07-12 10:42:04 +0530

    define globals

---
+++ globals.py
@@ -0,0 +1,20 @@
+# DO NOT CHANGE THIS FILE
+
+BATTING_CHOICES = [1, 2, 3, 4, 5, 6]
+BOWLING_CHOICES = [1, 2, 3, 4]
+
+batting_schema = {
+        1: 1, #one
+        2: 2, #two
+        3: 3, #three
+        4: 4, #four
+        5: 0, #out
+        6: 6  #six
+    }
+
+bowling_schema = {
+    1: 0, #ball
+    2: 1, #wide
+    3: 1, #no_ball
+    4: 0, #bouncer
+}
